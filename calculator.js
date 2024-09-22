ef8eb783ca9fab8a3c57dbe36f6874591541146d
albert912 <aatunckol@gmail.com>
2024-09-22 13:15:14 +0300

    Create various math operator functions.

---
+++ calculator.js
@@ -0,0 +1,60 @@
+function add(a, b) {
+   return a + b;
+}
+
+function substract(a, b) {
+
+ return a - b;
+}
+
+function multiply(a, b) {
+
+    return a*b;
+
+}
+
+function divide(a, b) {
+
+    return a/b;
+
+}
+
+//function add() {
+
+    //let firstnumber = a;
+
+    //let operator = +;
+
+    //let secondnumber = b;
+
+//}
+
+//function subtract() {
+
+   // let firstnumber = a;
+
+   // let operator = -;
+
+    //let secondnumber = b;
+
+//}
+
+//function multiply() {
+
+   // let firstnumber = a;
+
+   // let operator = *;
+
+  //  let secondnumber = b;
+
+//}
+
+//function divide() {
+
+   // let firstnumber = a;
+
+   // let operator = /;
+
+   // let secondnumber = b;
+
+//}
